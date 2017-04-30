4015cd455d37d14aacdea8914ac46ca167e08ca4
ironcolumn <403291934@qq.com>
2017-04-30 22:16:00 +0800

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProductComponent} from "./product/product.component";
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
-  }
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'product', component: ProductComponent
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
